35ed3346a01bb026575b2026a3a0239647dc8d4d
Nagendra07-moh <nagendra.mohan.5@gmail.com>
2024-06-23 17:34:03 +0530

    almost done

--- code/src/Screens/OnBoardingScreen.js
+++ code/src/Screens/OnBoardingScreen.js
@@ -5,20 +5,37 @@ import {
   StyleSheet,
   Image,
   Dimensions,
+  TouchableOpacity,
 } from "react-native";
 import React from "react";
 import Onboarding from "react-native-onboarding-swiper";
 import LottieView from "lottie-react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const { height, width } = Dimensions.get("window");
 const OnBoardingScreen = () => {
+  const navigation = useNavigation();
+  const handleDone = () => {
+    navigation.navigate("Home");
+  };
+  const done = ({ ...props }) => {
+    return (
+      <TouchableOpacity style={style.doneBtn} {...props}>
+        <Text style={{ fontWeight: "500" }}>Done</Text>
+      </TouchableOpacity>
+    );
+  };
   return (
     <View style={style.Container}>
       <Onboarding
+        onDone={handleDone}
+        onSkip={handleDone}
+        bottomBarHighlight={false}
+        DoneButtonComponent={done}
         containerStyles={{ paddingHorizontal: 15 }}
         pages={[
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#a7f3d0",
             image: (
               <View>
                 <LottieView
@@ -33,7 +50,7 @@ const OnBoardingScreen = () => {
             subtitle: "Hello World",
           },
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#fef3c7",
             image: (
               <View>
                 <LottieView
@@ -48,7 +65,7 @@ const OnBoardingScreen = () => {
             subtitle: "Done with React Native Onboarding Swiper",
           },
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#a78bfa",
             image: (
               <View>
                 <LottieView
@@ -63,7 +80,7 @@ const OnBoardingScreen = () => {
             subtitle: "Done with React Native Onboarding Swiper",
           },
           {
-            backgroundColor: "#fff",
+            backgroundColor: "#899E92",
             image: (
               <View>
                 <LottieView
@@ -93,5 +110,11 @@ const style = StyleSheet.create({
   lottie: {
     width: width * 0.9,
     height: height * 0.4,
+  },
+  doneBtn: {
+    padding: 20,
+    backgroundColor: "white",
+    borderTopLeftRadius: "100%",
+    borderBottomLeftRadius: "100%",
   },
 });
